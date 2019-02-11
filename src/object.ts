b286d90e419d0e9018e580e826f29f1ac757ff03
チセP <jeawon139@gmail.com>
2019-02-11 13:51:58 +0900

    Update object.ts

--- src/object.ts
+++ src/object.ts
@@ -10,3 +10,17 @@ let {ke1:a, ke3:b} = obj;
 
 console.log(a);
 console.log(b);
+
+/*
+object.js
+var obj = {
+    ke1: "hello, world",
+    ke2: 100,
+    ke3: "Type Script"
+};
+var a = obj.ke1, b = obj.ke3;
+//let { ke1:a, ke2:b } = obj;
+console.log(a);
+console.log(b);
+
+*/
