45d82ee1d2ece319442f9331dcc5633f941d2d52
チセP <jeawon139@gmail.com>
2019-02-11 13:54:53 +0900

    Add files via upload

---
+++ src/object2.ts
@@ -0,0 +1,12 @@
+let {a, b} = {a:"Hello World", b:100};
+
+console.log(a);
+console.log(b);
+
+/*
+object2.js
+var _a = { a: "Hello World", b: 100 }, a = _a.a, b = _a.b;
+
+console.log(a);
+console.log(b);
+*/
